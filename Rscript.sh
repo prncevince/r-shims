86f45bdb8dd34abd1d4ec6c7fb675b92ed69e9af
Vincent Clemson <vclemson07@gmail.com>
2020-12-14 00:33:41 -0500

    exit instead of return

--- Rscript.sh
+++ Rscript.sh
@@ -3,7 +3,7 @@ if [ -f .Rversion ]; then
   rpath=/Library/Frameworks/R.framework/Versions
   ver=$(/bin/cat .Rversion)
   if [[ $(/bin/ls "$rpath" | /usr/bin/grep "$ver") == "" ]]; then
-    echo "R version" "$ver" "not installed" && return 1
+    echo "R version" "$ver" "not installed" && exit
   else
     export R_HOME="$rpath"/"$ver"/Resources
   fi
