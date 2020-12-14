f9891a8b418a51d5ad4cc386413de2912324d7bc
Vincent Clemson <vclemson07@gmail.com>
2020-12-14 00:39:49 -0500

    use full paths as much as possible & exit instead of return

--- rstudio.sh
+++ rstudio.sh
@@ -1,12 +1,12 @@
 rpath=/Library/Frameworks/R.framework/Versions
-old=$(readlink "$rpath"/Current); 
+old=$(/usr/bin/readlink "$rpath"/Current); 
 if [ -f .Rversion ]; then 
-  new=$(cat .Rversion)
-  if [[ $(/bin/ls "$rpath" | grep "$new") == "" ]]; then 
-    echo "R version" "$new" "not installed" && return 1; else
-    ln -sfh "$new" "$rpath"/Current; fi; fi
+  new=$(/bin/cat .Rversion)
+  if [[ $(/bin/ls "$rpath" | /usr/bin/grep "$new") == "" ]]; then 
+    echo "R version" "$new" "not installed" && exit; else
+    /bin/ln -sfh "$new" "$rpath"/Current; fi; fi
 if [ -n "$ZSH_VERSION" ]; then unsetopt local_options nomatch; fi
 if [ -f *.Rproj ]; then 
-  open -na Rstudio *.Rproj; else 
+  /usr/bin/open -na Rstudio *.Rproj; else 
   ("/Applications/RStudio.app/Contents/MacOS/RStudio" &)
-fi; ({ sleep 5; ln -sfh "$old" "$rpath"/Current;} &)
+fi; ({ sleep 5; /bin/ln -sfh "$old" "$rpath"/Current;} &)
